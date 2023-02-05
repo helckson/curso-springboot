9148a086986e2402a31c5b79b7c9eaec21650cf5
helckson <helckson.ishibashi@hotmail.com>
2023-02-05 16:15:42 -0300

    first commit

---
+++ src/main/java/io/github/helckson/VendasApplication.java
@@ -0,0 +1,32 @@
+package io.github.helckson;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.helckson.domain.entity.Cliente;
+import io.github.helckson.domain.respositorio.Clientes;
+
+@SpringBootApplication
+public class VendasApplication {
+	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			clientes.salvar(new Cliente("Joao"));
+			clientes.salvar(new Cliente("José"));
+			
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApplication.class, args);
+
+	}
+}
