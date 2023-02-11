15af2bf1dc3ccb16d83c9ce995a1e8974ce2ff49
helckson <helckson.ishibashi@hotmail.com>
2023-02-11 20:19:35 -0300

    Working with EntityManager

--- src/main/java/io/github/helckson/domain/entity/Cliente.java
+++ src/main/java/io/github/helckson/domain/entity/Cliente.java
@@ -1,8 +1,22 @@
 package io.github.helckson.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "nome", length = 100)
 	private String nome;
 	
 	public Cliente() {
